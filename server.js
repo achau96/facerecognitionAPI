1e189c3b1f6c3a35cc00feccd3dd6ee6e1acda40
Austin Chau <austin.chau1@gmail.com>
2020-05-22 20:58:39 -0400

    Project files added

---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const knex = require('knex');
+const {DB_USER, DB_PASS, PORT, API_KEY} = process.env;
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : DB_USER,
+      password : DB_PASS,
+      database : 'smartbrain'
+    }
+  });
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.post('/signin', (req,res)=>{(signin.handleSignin(req,res,db,bcrypt))})
+
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req,res,db)})
+
+app.put('/image', (req,res) => {image.handleImage(req,res,db)})
+
+app.post('/imageurl', (req,res) => {image.handleApiCall(req,res,API_KEY)})
+
+app.listen(PORT, () => {
+    console.log('listening on port 3001')
+})
+
+
